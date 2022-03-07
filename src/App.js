3cb90d74e08014b8d8f06e2bca8322066ef20fbe
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-03-07 18:30:49 +0300

    Cart, Meals, Form and Modal components is added to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
 	return (
 		<div>
 			<React.Fragment>
+				<Cart />
 				<Header />
+				<main>
+					<Meals />
+				</main>
 			</React.Fragment>
 		</div>
 	);
