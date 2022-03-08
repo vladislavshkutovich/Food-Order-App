0bcbb9236bf8728cd32cb05d0150396c7ecbda39
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-03-08 12:11:43 +0300

    Opening/closing of modal window is implemented via useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	const showCartHandler = () => {
+		setCartIsShown(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	};
+
 	return (
 		<div>
 			<React.Fragment>
-				<Cart />
-				<Header />
+				{cartIsShown && <Cart onClose={hideCartHandler} />}
+				<Header onShowCart={showCartHandler} />
 				<main>
 					<Meals />
 				</main>
