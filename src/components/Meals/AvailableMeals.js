54ac57a30e91e4763bf5606d81e40f441b9a87d2
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-03-28 18:41:26 +0300

    Form validation is added via useState, useRef

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,19 @@ import classes from './AvailableMeals.module.css';
 // Fetching meals via HTTP (Firebase) is added
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
+	const [isLodaing, setIsLodaing] = useState(true);
+	const [httpError, setHttpError] = useState();
 
 	useEffect(() => {
 		const fetchMeals = async () => {
 			const response = await fetch(
 				'https://react-http-90041-default-rtdb.firebaseio.com/meals.json'
 			);
+
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+
 			const responseData = await response.json();
 
 			const loadedMeals = [];
@@ -27,10 +34,30 @@ const AvailableMeals = () => {
 			}
 
 			setMeals(loadedMeals);
-			console.log(loadedMeals);
+			setIsLodaing(false);
 		};
-		fetchMeals();
+
+		fetchMeals().catch((error) => {
+			setIsLodaing(false);
+			setHttpError(error.message);
+		});
 	}, []);
+
+	if (isLodaing) {
+		return (
+			<section className={classes.MealsLoading}>
+				<p>Lodaing...</p>
+			</section>
+		);
+	}
+
+	if (httpError) {
+		return (
+			<section className={classes.MealsError}>
+				<p>{httpError}</p>
+			</section>
+		);
+	}
 
 	const mealsList = meals.map((meal) => (
 		<MealItem
